3a572f8367815b039b7d989e9f3a5f77a4c59b0b
Liqaing <huotrithy01@gmail.com>
2023-06-06 21:55:04 +0700

    Checking error for add listing

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -3,6 +3,8 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
 
 from .models import User, item, bid, comment
 
@@ -70,6 +72,24 @@ def create_listing(request):
         description = request.POST["description"]
         starting_bid = request.POST["starting_bid"]
         image_url = request.POST["image_url"]
+
+        # Validate data that user submit
+        error_dict = {}
+        
+        if not title:
+            error_dict["missing_title"] = "Listing title is missing"
+        if not description:
+            error_dict["missing_description"] = "Listing description is missing"
+        if not starting_bid:
+            error_dict["missing_starting_bid"] = "Listing starting bid is missing"
+
+        # Validate url of image_url
+        validate_url = URLValidator(verify_exists=True)
+        try:
+            validate_url(image_url)
+        except ValidationError:
+            error_dict["invalid_image_url"] = "Invalid Url For Image"
+
 
         # Create item object and add its to the item database
         new_item = item(title=title, description=description, starting_bid=starting_bid, image_url=image_url)
